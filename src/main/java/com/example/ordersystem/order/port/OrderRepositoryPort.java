048f41e43624f1dac076803a21f0a708c1ece909
Seungwon-Choi <bestwondev@gmail.com>
2026-01-29 14:18:10 +0900

    feat: OrderRepositoryPort 추가

---
+++ src/main/java/com/example/ordersystem/order/port/OrderRepositoryPort.java
@@ -0,0 +1,15 @@
+package com.example.ordersystem.order.port;
+
+import com.example.ordersystem.order.domain.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepositoryPort {
+
+    Order save(Order order);
+
+    Optional<Order> findById(Long orderId);
+
+    List<Order> findAll();
+}
