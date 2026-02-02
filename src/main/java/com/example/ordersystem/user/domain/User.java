56ed1782cc905b71c888afc18ec9483c5b8abc0a
Seungwon-Choi <bestwondev@gmail.com>
2026-02-02 17:35:33 +0900

    feat: User 엔티티 클래스 구현

---
+++ src/main/java/com/example/ordersystem/user/domain/User.java
@@ -0,0 +1,29 @@
+package com.example.ordersystem.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
