164dcdaa0efeff75d518e2acaf4b07b9f23ed9cd
Seungwon-Choi <bestwondev@gmail.com>
2026-02-02 19:05:59 +0900

    feat: UpdateProductRequest Validation 어노테이션 추가

--- src/main/java/com/example/ordersystem/product/application/dto/request/UpdateProductRequest.java
+++ src/main/java/com/example/ordersystem/product/application/dto/request/UpdateProductRequest.java
@@ -1,11 +1,26 @@
 package com.example.ordersystem.product.application.dto.request;
 
 import com.example.ordersystem.product.application.dto.command.UpdateProductCommand;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record UpdateProductRequest(
+        @NotBlank(message = "상품명은 필수입니다.")
+        @Size(max = 100, message = "상품명은 100자를 초과할 수 없습니다.")
         String name,
+
+        @Size(max = 1000, message = "상품 설명은 1000자를 초과할 수 없습니다.")
         String description,
+
+        @NotNull(message = "가격은 필수입니다.")
+        @DecimalMin(value = "0.01", message = "가격은 0보다 커야 합니다.")
         Double price,
+
+        @NotNull(message = "재고 수량은 필수입니다.")
+        @PositiveOrZero(message = "재고 수량은 0 이상이어야 합니다.")
         Integer stock
 ) {
     public UpdateProductCommand toCommand() {
